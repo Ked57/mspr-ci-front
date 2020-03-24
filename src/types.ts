4361634f70ce574824022ae741615b538144c1aa
Clément Fassot <kedketh@gmail.com>
2020-03-24 14:48:53 +0100

    :white_check_mark: Add store unit testing (#13)

--- src/types.ts
+++ src/types.ts
@@ -1,17 +1,33 @@
 export type Product = {
+  id: string;
   name: string;
-  unitPrice: number;
+  price: number;
   description: string;
   category: string;
-  color: string;
+  color?: string;
   logoUrl: string;
   vatType: string;
 };
+
+export const isProduct = (product: any): product is Product =>
+  product &&
+  typeof product.id === "string" &&
+  typeof product.name === "string" &&
+  typeof product.price === "number" &&
+  typeof product.description === "string" &&
+  typeof product.category === "string" &&
+  typeof product.logoUrl === "string" &&
+  typeof product.vatType === "string";
 
 export type User = {
   auth_id: string;
   user_name: string;
 };
+
+export const isUser = (user: any): user is User =>
+  user &&
+  typeof user.auth_id === "string" &&
+  typeof user.user_name === "string";
 
 export type Cart = {
   createdAt: Date;
---
+++ tests/unit/store.spec.ts
@@ -0,0 +1,76 @@
+import store from "@/store/index";
+import { ACTIONS } from "@/store/actions-definition";
+import { isProduct, isUser } from "@/types";
+
+describe("store", () => {
+  it("action gets and sets products", async () => {
+    await store.dispatch(ACTIONS.SET_PRODUCTS);
+    expect(store.state.products.every(p => isProduct(p)));
+  });
+  it("action gets and sets user when already exists", async () => {
+    await store.dispatch(ACTIONS.SET_USER, "user@example.com");
+    expect(
+      isUser(store.state.user) &&
+        store.state.user.auth_id === "user@example.com"
+    );
+  });
+  it("action gets and sets user when doesn't exists", async () => {
+    await store.dispatch(ACTIONS.SET_USER, "user2@example.com");
+    expect(
+      isUser(store.state.user) &&
+        store.state.user.auth_id === "user2@example.com"
+    );
+  });
+  it("action adds product to cart", () => {
+    store.dispatch(ACTIONS.ADD_PRODUCT_TO_CART, {
+      id: "qskfqksfnqgnqnkgqsre",
+      name: "Peace Lily",
+      price: 30,
+      description: "A nice plant",
+      category: "Indoor Plant",
+      color: "red",
+      vatType: "normal",
+      logoUrl:
+        "https://user-images.githubusercontent.com/2805249/64069899-8bdaa180-cc97-11e9-9b19-1a9e1a254c18.png"
+    });
+    expect(
+      store.state.cart &&
+        store.state.cart.products.length > 0 &&
+        store.state.cart.products.find(
+          p => p.product.id === "qskfqksfnqgnqnkgqsre"
+        )?.quantity === 1 &&
+        store.state.cart.products.every(p => isProduct(p.product))
+    );
+  });
+  it("action removes product from cart", () => {
+    store.dispatch(ACTIONS.ADD_PRODUCT_TO_CART, {
+      id: "qosfqognqjnunegbgdshgbhdg",
+      name: "Hand Sanitizer",
+      price: 3000,
+      description: "Rare remaining of hand sanitizer",
+      category: "Healthcare",
+      color: "blue",
+      vatType: "intermediary",
+      logoUrl:
+        "https://mspr-ci-products.s3.fr-par.scw.cloud/person-holding-hand-sanitizer-3962331-removebg-preview.png"
+    });
+    store.dispatch(ACTIONS.REMOVE_PRODUCT_FROM_CART, {
+      id: "qosfqognqjnunegbgdshgbhdg",
+      name: "Hand Sanitizer",
+      price: 3000,
+      description: "Rare remaining of hand sanitizer",
+      category: "Healthcare",
+      color: "blue",
+      vatType: "intermediary",
+      logoUrl:
+        "https://mspr-ci-products.s3.fr-par.scw.cloud/person-holding-hand-sanitizer-3962331-removebg-preview.png"
+    });
+    expect(
+      store.state.cart &&
+        store.state.cart.products.length <= 0 &&
+        !store.state.cart.products.find(
+          p => p.product.id === "qosfqognqjnunegbgdshgbhdg"
+        )
+    );
+  });
+});
